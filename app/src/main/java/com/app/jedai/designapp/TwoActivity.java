e2fdd4e089e7d49eaf964056ee25d4f5f9f0b35d
luke <nikolas>
2016-06-08 22:20:53 +0300

    first commit

---
+++ app/src/main/java/com/app/jedai/designapp/TwoActivity.java
@@ -0,0 +1,16 @@
+package com.app.jedai.designapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by artem on 06.06.16.
+ */
+public class TwoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two_activity);
+    }
+}
